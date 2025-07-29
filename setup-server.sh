6e81190c48e9d932926258ef748494faa0fbfe76
n0kri <nokri7118@gmail.com>
2025-07-29 16:24:46 +0300

    Update setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -72,14 +72,14 @@ cat > start.sh <<'EOL'
 JVM_FLAGS="-Xms3G -Xmx3G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
 
 # Запуск сервера (без цикла перезапуска)
-echo "Starting Minecraft server..."
+echo "Запускаем minecraft сервер..."
 exec java ${JVM_FLAGS} -jar paper.jar nogui
 EOL
 
 # 10. Настройка server.properties (без RCON)
 cat > server.properties <<EOL
 #Minecraft server properties
-spawn-protection=16
+spawn-protection=0
 max-tick-time=30000
 server-port=25565
 server-ip=
